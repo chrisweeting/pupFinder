c324aa6bc1518b747642b1ab3c2b1335456439b4
chrisweeting <chrisweeting@gmail.com>
2021-07-15 23:30:06 -0400

    Configure graphql

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
 const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
 const app = express();
+const mongoose = require('mongoose');
+const schema = require('./schema/schema');
+const db = require('./config/keys').mongoURI;
+const users = require("./routes/api/users");
+const pups = require("./routes/api/pups");
+
+mongoose
+  .connect(db, { useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify: false })
+  .then(() => console.log("Connected to DB"))
+  .catch((err) => console.log(err));
 
 // app listens for get request on "/"
-app.get("/", (req, res) => {
-  res.send("hello world?");
-});
+// app.get("/", (req, res) => {
+//   res.send("test");
+// });
+
+app.use("/graphql", graphqlHTTP({
+  schema,
+  graphiql: true
+}));
+app.use("/api/users", users );
+app.use("/api/pups", pups );
 
 // port var set to port 5000 or PORT in env file
 const port = process.env.PORT || 5000;
---
+++ schema/schema.js
@@ -0,0 +1,82 @@
+const graphql = require("graphql");
+const { _ } =require("lodash");
+const { 
+  GraphQLObjectType, 
+  GraphQLString,
+  GraphQLInt,
+  GraphQLFloat,
+  GraphQLSchema 
+} = graphql;
+
+//test data
+
+const pups = [
+  {
+    id: "001",
+    name: "Maui",
+    breed: "Labrador Retriever",
+    age: 1,
+    status: "Available",
+    title: "Meet Maui!",
+    description: "Meet Maui! Our handsome, black and white Labrador/Spaniel mix! Maui is a 42 lb, 19 month old boy. He has a sweet and loving face, and is searching for his forever home! Maui will do best with an experienced dog owner who has the time to teach him the ways of the world. He is highly intelligent and needs an owner to match his physical energy and brainpower. Maui is a friendly boy who would do well in a home with other dogs and children 12yo and above but has not been introduced to a cat. He is a confident boy with a medium/high energy level and will do best in a home with a fenced yard. His ideal home will be able to provide him with a clear routine and well structured life. He likes to play outside, lay in your lap or go for a good car ride.Maui is crate and house trained. He is almost leash trained and will need some more time and effort to master this skill. Maui is an intelligent boy who looks forward to continue to learn some more advanced commands with his new owner. Maui is a beautiful boy with a smiley face. He would make an excellent addition to any family that can provide him with a loving and playful home. Mauis adoption fee is $450. That includes the cost of spaying, up to date vaccinations, foster care, rescue, and transport fees. To get the ball rolling, head over to our Adoption Application and submit your application today. A Dog Star representative will contact you within 24 hours. To help speed up the process contact your vet and give them permission to speak with us. If you do not get a response within 24 hours of submitting your application, please check your spam folder.",
+    size: "Medium",
+    color: "White",
+    gender: "Male",
+    city: "Bloomfield",
+    state: "CT",
+    fee: 0.00
+  },
+  {
+    id: "002",
+    name: "Zach",
+    breed: "German Shepherd",
+    age: 1,
+    status: "Available",
+    title: "Meet Zach",
+    description: "Zach is one of the sweetest guys you will ever meet. He is about 2 years old and is a perfect size at around 40 lbs. He came to us from South Carolina and he was intially very timid. He is quickly coming out of his shell and is getting more confident and friendly by the day. Zach is exceptional around all other dogs and takes cues from them incredibly well. He is submissive and non threatening and other dogs like him instantly. He is also good with all people as long as they don't approach him too quickly. He will just avoid you until he is comfortable. We are looking for someone who is quiet, patient and calm to help with his confidence and to show him all the great things the world has to offer and he will blossom. We woulld love to find him a home with another dog would since that is very helpful for his confidence. He LOVES to be outside and he has great manners in the home. Once he feels comfortable you will see his adorable playful side come out. He also loves children and seems soothed by their innocent nature. Zach is 99% housebroken as long as you are on top of it. He will need a consistent routine and a lot of positive reinforcement to complete his training. Sudden movements and loud noises scare him so a quieter home is essential. If you feel like you are the right family for Zach, please fill out an application. We are currently only doing adoptions within CT at this time. Thank you!",
+    size: "Medium",
+    color: "Black",
+    gender: "Male",
+    city: "Waterbury",
+    state: "CT",
+    fee: 325.00
+  },
+];
+
+const PupType = new GraphQLObjectType({
+  name: "Pup",
+  fields: () => ({
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    breed: { type: GraphQLString },
+    age: { type: GraphQLInt },
+    status: { type: GraphQLString },
+    title: { type: GraphQLString },
+    description: { type: GraphQLString },
+    size: { type: GraphQLString },
+    color: { type: GraphQLString },
+    gender: { type: GraphQLString },
+    city: { type: GraphQLString },
+    state: { type: GraphQLString },
+    fee: { type: GraphQLFloat },
+    // owner: { type: UserType }
+  })
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: {
+    Pup: {
+      type: PupType,
+      args: { id: { type: GraphQLString } },
+      resolve(parent, args) {
+        //code to get data from db
+        return _.find(pups, {id: args.id});
+      }
+    }
+  }
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
+});
