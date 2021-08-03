17e02c2afe422dfc5dabc07619bdb149ea51c7ba
chrisweeting <chrisweeting@gmail.com>
2021-08-03 00:04:45 -0400

    Connect root to index

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,30 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Root from './components/root';
+import configureStore from './store/store';
 import reportWebVitals from './reportWebVitals';
 
 document.addEventListener("DOMContentLoaded", () => {
   let store;
 
-  if (localStorage.jwtToken) {
-    setAuthToken(localStorage.jwtToken);
-
-    const decodedUser = jwt_decode(localStorage.jwtToken);
-
-    const preloadedState = {
-      session: { isAuthenticated: true, user: decodedUser },
-    };
-
-    store = configureStore(preloadedState);
-
-    const currentTime = Date.now() / 1000;
-    if (decodedUser.exp < currentTime) {
-      store.dispatch(logout());
-      window.location.href = "/";
-    }
-  } else {
-    store = configureStore({});
-  }
+  store = configureStore();
 
   window.getState = store.getState;
 
